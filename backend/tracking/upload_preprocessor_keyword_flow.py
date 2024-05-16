f78e4ec56de0cf238739385c5fd7cdf4faa5a685
Lander Van der Stighelen <lander@vanderstighelen.net>
2024-05-16 16:42:43 +0200

    Make a flow to upload the train data and keywords to the database

--- backend/tracking/upload_preprocessor_keyword_flow.py
+++ backend/tracking/upload_preprocessor_keyword_flow.py
@@ -1,14 +1,15 @@
 import json
 from pymongo import MongoClient
 from os import environ as e
-def upload_test_data():
-    def open_file(training_set_file):
-        with open(file=training_set_file, mode='r', encoding='utf-8') as f:
-            m = json.load(f)
-        return m
 
-    def upload_to_database(mail):
-        mongo_host = e.get('MONGO_HOST', 'localhost')
+def upload_test_data():
+    def open_file(file_path):
+        with open(file=file_path, mode='r', encoding='utf-8') as f:
+            data = json.load(f)
+        return data
+
+    def upload_to_database(data, collection_name):
+        mongo_host = e.get('MONGO_HOST', 'db')
         mongo_port = int(e.get('MONGO_PORT', '27017'))
         mongo_username = e.get('MONGO_USERNAME', 'root')
         mongo_password = e.get('MONGO_PASSWORD', 'mongo')
@@ -16,4 +17,19 @@ def upload_test_data():
         client = MongoClient(host=mongo_host, port=mongo_port, username=mongo_username, password=mongo_password)
 
         db = client['filtrr_db']
+        collection = db[collection_name]
+        if isinstance(data, list):
+            collection.insert_many(data)
+        else:
+            collection.insert_one(data)
 
+    train_data = open_file("./data.json")
+    keywords = open_file("./keywords.json")
+    parameters = open_file("./parameters.json")
+
+    upload_to_database(train_data, collection_name="train_data")
+    upload_to_database(keywords, collection_name="keywords")
+    upload_to_database(parameters, collection_name="parameters")
+
+
+upload_test_data()
