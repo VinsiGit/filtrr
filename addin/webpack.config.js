ba9cff068ef733549444a24f03066672d19042fa
Vinsi <40694625+VinsiGit@users.noreply.github.com>
2024-05-31 17:14:39 +0200

    made a mistake in the addin config file

--- addin/webpack.config.js
+++ addin/webpack.config.js
@@ -4,7 +4,7 @@ const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const urlDev = "https://s144272.devops-ap.be/addin"; //localhost.be
+const urlDev = "https://localhost:3000"; // moet https://localhost:3000 zijn 
 const urlProd = "https://s144272.devops-ap.be/addin"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
